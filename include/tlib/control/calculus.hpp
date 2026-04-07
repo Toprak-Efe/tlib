0f268549d074ffcb463282deb3003fc5636d7d50
Toprak <efe.akkilic@ozu.edu.tr>
2026-04-08 00:43:20 +0300

    Updated and rectified the calculus types.

--- include/tlib/control/calculus.hpp
+++ include/tlib/control/calculus.hpp
@@ -1,18 +1,20 @@
 #pragma once
 
+#include <cmath>
 #include <tlib/control/concepts/holdable.hpp>
 
 template <Holdable T> class Differentiator {
 public:
-  T operator()(const T &data) {
-    auto dt = std::chrono::duration<double>(data.stamp() - x1_.stamp()).count();
+  explicit Differentiator(double tau) : tau_(tau) {}
 
-    T out{};
-    if (dt > 1e-9) {
-      out = (data - x1_) / dt;
-      out.stamp() = data.stamp();
-    }
-    x1_ = data;
+  T operator()(const T &x) {
+    auto dt = std::chrono::duration<double>(x.stamp() - x1_.stamp()).count();
+
+    T out((x - x1_ - y1_ * (dt / 2.0 - tau_)) / (tau_ + dt / 2.0));
+    out.stamp() = x.stamp();
+
+    x1_ = x;
+    y1_ = out;
     return out;
   }
 
@@ -20,30 +22,47 @@ public:
     T zero{};
     zero.stamp() = x1_.stamp();
     x1_ = zero;
+    zero.stamp() = y1_.stamp();
+    y1_ = zero;
   }
 
 private:
+  double tau_;
   T x1_{};
-  bool first_{true};
+  T y1_{};
 }; // Differentiator
 
 template <Holdable T> class DecayingIntegrator {
 public:
-  explicit DecayingIntegrator(double decay = 0.995) : decay_(decay) {}
+  explicit DecayingIntegrator(double decay_per_sec = 0.5)
+      : decay_(decay_per_sec), init_(false) {}
 
   T operator()(const T &data) {
+    if (!init_) {
+      x1_ = data;
+      init_ = true;
+      accum_.stamp() = data.stamp();
+      return accum_;
+    }
+
     auto dt = std::chrono::duration<double>(data.stamp() - x1_.stamp()).count();
 
     if (dt > 1e-9) {
-      accum_ *= decay_;
-      accum_ += (data + x1_) * (0.5 * dt);
+      accum_ = accum_ * pow(decay_, dt) + (data + x1_) * 0.5 * dt;
       accum_.stamp() = data.stamp();
     }
     x1_ = data;
     return accum_;
   }
 
+  void reset() {
+    x1_ = T{};
+    accum_ = T{};
+    init_ = false;
+  }
+
 private:
+  bool init_;
   double decay_;
   T x1_{};
   T accum_{};
